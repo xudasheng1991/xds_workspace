e6b514bb133fee854dc4e429322b914b69f4263c
dndxxds <dndxxds@163.com>
2020-11-29 23:07:51 +0800

    通过服务发现来获得该服务的信息

--- mycloud2020/cloud-provider-payment8001/src/main/java/com/xds/springcloud/controller/PaymentController.java
+++ mycloud2020/cloud-provider-payment8001/src/main/java/com/xds/springcloud/controller/PaymentController.java
@@ -6,10 +6,15 @@ import com.xds.springcloud.service.PaymentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @program: mycloud2020
@@ -21,6 +26,8 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/payment")
 public class PaymentController {
+    @Resource
+    private DiscoveryClient discoveryClient;
     @Value("${server.port}")
     private String serverPort;
     @Autowired
@@ -50,5 +57,26 @@ public class PaymentController {
        }
 
     }
+
+
+
+
+    @RequestMapping(value = "/discovery")
+    public Object discovery(){
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("***** element:"+element);
+        }
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t"+instance.getHost()+"\t"+instance.getPort()+"\t"+instance.getUri());
+        }
+        return this.discoveryClient;
+    }
+
+
+
+
+
 
 }
